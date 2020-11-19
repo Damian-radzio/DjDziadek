5d6924adf388f5757f391764819323b2333edfdd
Damian-Radzio <drado2003@gmail.com>
2020-11-19 19:11:50 +0100

    first commit

---
+++ scripts/main/main.js
@@ -0,0 +1,90 @@
+// menubar with animations on scroll 
+const nameBar = document.querySelector('.name');
+const menuIcon = document.querySelector('.wrap');
+const menuWrap = document.querySelector('.header');
+
+function MinimalizeMenuBar(){
+  if(scrollY > 0){
+    nameBar.classList.add('active')
+    menuIcon.classList.add('active');
+    menuWrap.classList.add('active');
+  }else{
+    nameBar.classList.remove('active');
+    menuIcon.classList.remove('active');
+    menuWrap.classList.remove('active');
+  }
+}
+window.addEventListener('scroll', MinimalizeMenuBar)
+
+
+// typing effect header 
+
+
+setTimeout(() => {
+  const text = "Przy mnie każda impreza będzie niezapomniana! Dj Dziadek"
+let i = 0;
+let speed = 50;
+  (function TypeWriter(){
+    if(i< text.length){
+      const textHeader = document.querySelector('.about-intro .text-intro')
+      textHeader.innerHTML += text.charAt(i);
+      i++;
+      setTimeout(TypeWriter, speed);
+      if(textHeader.textContent === text){
+        const buttonHeader = document.querySelector('.about-btn');
+          buttonHeader.classList.add('visible');
+      }
+    }
+  })();
+}, 500);
+
+
+
+
+
+// experience counter
+const updateCount = () => {
+  const experienceNumber = document.querySelector('.experience-number');
+  let value = 7;
+  let i = 0;
+  setInterval(() => {
+    experienceNumber.textContent = i;
+    if(i<value){
+      i++;
+      if(i === 3){
+        experienceNumber.classList.add('active')
+      }
+    }
+  }, 200);
+}
+
+const checksScrollPos = () => {
+  if(window.scrollY >= window.innerHeight/3){
+    updateCount();
+    window.removeEventListener('scroll', checksScrollPos, false)
+  }
+}
+
+window.addEventListener('scroll', checksScrollPos)
+
+
+// about me blur animation
+const experienceSection = document.querySelector('.experience');
+
+const aboutMeBlur = () => {
+  const backgroundAboutMe = document.querySelector('.about-me .background');
+  if(window.scrollY > window.innerHeight +  experienceSection.scrollHeight / 2){
+    backgroundAboutMe.classList.add('active');
+  }else if(window.scrollY < window.innerHeight +  experienceSection.scrollHeight / 2 || window.scrollY > window.innerHeight +  experienceSection.scrollHeight){
+    backgroundAboutMe.classList.remove('active');
+  }
+
+}
+
+window.addEventListener('scroll', aboutMeBlur);
+
+
+
+
+
+
