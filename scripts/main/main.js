c20d4c2dd42e3767330efa8b57aec41ca58cb484
Damian-Radzio <drado2003@gmail.com>
2020-12-01 11:36:11 +0100

    add messenger

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -158,6 +158,15 @@ HeaderName.addEventListener('click', backToTop)
 
 
 
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+
 
 
 
