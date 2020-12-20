458dea09e79ef2a5266886b26c014312adb010c6
Damian-Radzio <drado2003@gmail.com>
2020-12-20 22:22:28 +0100

    add preload

--- scripts/main/main.js
+++ scripts/main/main.js
@@ -1,41 +1,46 @@
+const experienceSection = document.querySelector('.experience');
+const evenIconOffer = [...document.querySelectorAll('.types .even')];
+const oddIconOffer = [...document.querySelectorAll('.types .odd')];
+const sectionOffer = document.querySelector('.offer')
+
 // menubar with animations on scroll 
+function MinimalizeMenuBar(menuContent,  menuContentUl, menuContentText, menuHeader, menuIcon, nameBar){
+const items = [
+  menuContent = document.querySelector('.menu-content'),
+  menuContentUl = document.querySelector('.menu-content .wrapper ul'),
+  menuContentText = document.querySelectorAll('.menu-content .wrapper ul li a'),
+  menuHeader = document.querySelector('.header'),
+  menuIcon = document.querySelector('.wrap'),
+  nameBar = document.querySelector('.name'),
+]
 
-
-function MinimalizeMenuBar(menuContent,  menuContentUl, menuContentText, menuWrap, menuIcon, nameBar){
-
- menuContent = document.querySelector('.menu-content');
- menuContentUl = document.querySelector('.menu-content .wrapper ul');
- menuContentText = document.querySelectorAll('.menu-content .wrapper ul li a');
- menuWrap = document.querySelector('.header');
- menuIcon = document.querySelector('.wrap');
- nameBar = document.querySelector('.name');
 
   if(scrollY > 0){
-    menuWrap.classList.add('active');
-    nameBar.classList.add('active')
-    menuIcon.classList.add('active');
+   items[3].classList.add('active');
+    items[items.length-1].classList.add('active')
+    items[items.length-2].classList.add('active');
 
     if(window.innerWidth >= 1024){
-      menuContent.classList.add('scroll');
+      items[0].classList.add('scroll');
     }
 
     if(window.innerWidth < 1024){
-      menuContent.classList.add('scrollMobile');
-      menuContentUl.classList.add('scrollMobile')
+      items[0].classList.add('scrollMobile');
+      items[1].classList.add('scrollMobile')
 
-      menuContentText.forEach(a => {
+      items[2].forEach(a => {
         a.classList.add('scrollMobile')
       });
     }
 
   }else{
-    menuWrap.classList.remove('active');
-    nameBar.classList.remove('active');
-    menuIcon.classList.remove('active');
-    menuContent.classList.remove('scroll');
-    menuContent.classList.remove('scrollMobile');
-    menuContentUl.classList.remove('scrollMobile')
-    menuContentText.forEach(a => {
+    items[0].classList.remove('active');
+    items[0].classList.remove('scroll');
+    items[0].classList.remove('scrollMobile');
+    items[3].classList.remove('active');
+    items[items.length-2].classList.remove('active');
+    items[1].classList.remove('scrollMobile')
+    items[2].forEach(a => {
       a.classList.remove('scrollMobile')
     });
   }
@@ -43,15 +48,18 @@ function MinimalizeMenuBar(menuContent,  menuContentUl, menuContentText, menuWra
 window.addEventListener('scroll', MinimalizeMenuBar)
 
 // typing effect header 
+
 setTimeout(() => {
-  const text = `Przy mnie każda impreza będzie niezapomniana! Dj-Dziadek`
-let i = 0;
-let speed = 10;
+  let i = 0;
+  const speed = 10;
+  const text = `Przy mnie każda impreza będzie niezapomniana! Dj-Dziadek`;
+  
   (function TypeWriter(){
     if(i< text.length){
       const textHeader = document.querySelector('.about-intro .text-intro')
       textHeader.innerHTML += text.charAt(i);
       i++;
+
       setTimeout(TypeWriter, speed);
       if(textHeader.textContent === text){
         const buttonHeader = document.querySelector('.about-btn');
@@ -87,16 +95,13 @@ const checksScrollPos = () => {
     window.removeEventListener('scroll', checksScrollPos, false)
   }
 }
-
 window.addEventListener('scroll', checksScrollPos)
 
 
-const experienceSection = document.querySelector('.experience');
 
 
-const evenIconOffer = [...document.querySelectorAll('.types .even')];
-const oddIconOffer = [...document.querySelectorAll('.types .odd')];
-const sectionOffer = document.querySelector('.offer')
+
+
 let time = 100;
 const offerIconScrollAnimation = () => {
   if(window.scrollY > window.innerHeight + experienceSection.scrollHeight + sectionOffer.scrollHeight / 2){
@@ -114,9 +119,9 @@ const offerIconScrollAnimation = () => {
     }, time);
   } 
 }
-
-
 window.addEventListener('scroll', offerIconScrollAnimation)
+
+
 // about Me button scroll animation
 const AboutMeButton = $('.about-btn');
 const moveToAboutMe = (e) => {
@@ -126,6 +131,7 @@ const moveToAboutMe = (e) => {
   }, 100)
 }
 AboutMeButton.click(moveToAboutMe);
+
 // menu after click
 // about me
 const AboutMeMenuBtn = $('.menu-about-me');
@@ -140,6 +146,8 @@ const moveToOffer = (e) => {
   })
 }
 OfferMenuBtn.click(moveToOffer);
+
+
 // gallery
 const moveToGallery = (e) => {
   e.preventDefault();
@@ -149,17 +157,30 @@ const moveToGallery = (e) => {
 }
 const galleryMenuBtn =  $('.menu-gallery');
 galleryMenuBtn.click(moveToGallery);
+
+
 // contact (menu btn & icon)
-const contactMenuBtn = $('.menu-contact');
-const contactIcon = $('.contact-icon');
 
 const moveToContact = () => {
   $('html, body').animate({
     scrollTop: $('.contact').offset().top
   }, 100)
 }
+const contactMenuBtn = $('.menu-contact');
+const contactIcon = $('.contact-icon');
+
 contactMenuBtn.click(moveToContact);
 contactIcon.click(moveToContact);
+
+
+// back to top Animation
+const heroeName = document.querySelector('.name');
+const backToTop = () => {
+   $('html, body').animate({
+     scrollTop: $('.main-header').offset().top
+   }, 200)
+}
+heroeName.addEventListener('click', backToTop)
 
 // // photos in gallery animation
 // const imageAnimation = (gallerySection, images) => {
@@ -212,14 +233,7 @@ const checkOrientation = () => {
 }
 window.addEventListener('orientationchange', checkOrientation)
 
-// back to top Animation
-const heroeName = document.querySelector('.name');
-const backToTop = () => {
-   $('html, body').animate({
-     scrollTop: $('.main-header').offset().top
-   }, 200)
-}
-heroeName.addEventListener('click', backToTop)
+
 
 
 
